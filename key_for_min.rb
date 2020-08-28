3e3a06d5b93b98aa6bb5df836feb3af929e073a7
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-28 08:08:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  empty = 10000
+  name_hash.each do |key, value|
+    if value < empty 
+      empty = value
+    end
+  end
+  puts key[empty]
 end
